a4d72eb07f48d26e26069801f13abd0027ac92b2
ehime <dodomeki@gmail.com>
2016-10-26 10:58:11 -0700

    Removing legacy ruby versions, terming dnsmasq and ngnix

--- modules/people/manifests/ehime.pp
+++ modules/people/manifests/ehime.pp
@@ -1,5 +1,16 @@
-class people::ehime {
+## Class people::ehime
+#
+class people::ehime { # "ehime" is YOUR GitHub username
 
   notify { 'class people::ehime declared': }
 
+  ## Remove services we don't want
+  #
+  service {"dev.nginx":
+      ensure => "stopped",
+  }
+
+  service {"dev.dnsmasq":
+      ensure => "stopped",
+  }
 }
